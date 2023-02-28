f5bc4baad01f4b630337df9d8c0745604066ca8a
Imrankhan <immzkhan532@gmail.com>
2023-02-28 20:10:25 +0530

    Js File

---
+++ Projects/Javascript/first.js
@@ -0,0 +1,19 @@
+// 7;
+// 2.5;
+// // Number Values
+
+let firstname = "Imran";
+let lastname = "khan";
+const mobile = 123456789;
+
+// console.log(firstname);
+// console.log(lastname);
+// console.log(mobile);
+
+// console.log("My First Name Is"+ firstname);
+// console.log("My Last Name IS ", lastname);
+// console.log(firstname,lastname,"my Number is",mobile)
+
+// Tempate Literal
+
+console.log(`My Current First Name Is ${firstname} ${lastname} ${mobile}`);
